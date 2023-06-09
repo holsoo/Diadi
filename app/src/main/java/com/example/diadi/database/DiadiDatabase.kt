9f440b1f15738d2c50c1496c47025f73f05d9fd2
wonjunYou <youwjune@gmail.com>
2023-06-09 10:58:25 +0900

    feat : 빌드 오류 수정, 이미지 표시 기능 구현

--- app/src/main/java/com/example/diadi/database/DiadiDatabase.kt
+++ app/src/main/java/com/example/diadi/database/DiadiDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.diadi.common.converter.Converters
 import com.example.diadi.dao.DiaryDao
 import com.example.diadi.dao.PlaceDao
 import com.example.diadi.dao.UserDao
@@ -12,13 +14,14 @@ import com.example.diadi.domain.Place
 import com.example.diadi.domain.User
 
 @Database(entities = [User::class, Diary::class, Place::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class DiadiDatabase : RoomDatabase() {
     abstract fun userDao() : UserDao
     abstract fun diaryDao(): DiaryDao
     abstract fun placeDao(): PlaceDao
 
     companion object {
-        private var instance: DiadiDatabase ?= null
+        @Volatile private var instance: DiadiDatabase ?= null
 
         fun getInstance(context: Context) : DiadiDatabase {
             return instance ?: synchronized(this) {
@@ -28,7 +31,7 @@ abstract class DiadiDatabase : RoomDatabase() {
 
         @Override
         private fun buildDatabase(context: Context) : DiadiDatabase {
-            return Room.databaseBuilder(context.applicationContext, DiadiDatabase::class.java, "diadi-database").build();
+            return Room.databaseBuilder(context.applicationContext, DiadiDatabase::class.java, "diadi-database").build()
         }
     }
 }
--- app/src/main/java/com/example/diadi/domain/User.kt
+++ app/src/main/java/com/example/diadi/domain/User.kt
@@ -3,16 +3,16 @@ package com.example.diadi.domain
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.LocalDateTime
+import java.util.Date
 
-@Entity
+@Entity(tableName = "user")
 data class User(
 
     @PrimaryKey(autoGenerate = true)
-    val userId : Long,
+    val userId : Int = 0,
 
     val nickname : String,
 
     @ColumnInfo(name = "created_at")
-    val createdAt: LocalDateTime = LocalDateTime.now()
+    val createdAt: Date = Date()
 )
