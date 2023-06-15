90226bf87a69973c32bbd7e10064a03d0079de2e
KimHyeyeonn <85014114+KimHyeyeonn@users.noreply.github.com>
2023-06-15 17:15:40 +0900

    수정하기, 삭제하기 구현

--- app/src/main/java/com/example/diadi/view/IndividualActivity.kt
+++ app/src/main/java/com/example/diadi/view/IndividualActivity.kt
@@ -2,13 +2,16 @@ package com.example.diadi.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diadi.R
 import com.example.diadi.databinding.ActivityIndividualBinding
 
 class IndividualActivity : AppCompatActivity() {
-    lateinit var binding : ActivityIndividualBinding
+
+    lateinit var binding: com.example.diadi.databinding.ActivityIndividualBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +28,30 @@ class IndividualActivity : AppCompatActivity() {
         }
 
         deleteButton.setOnClickListener {
-
+            // 여기에 일기 삭제 로직을 구현하세요.
+            // 삭제 후 필요한 작업을 수행하세요.
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       menu?.add(0,1,0,"수정하기")
+        menu?.add(0,2,0,"삭제하기")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_modify -> {
+                val modifyIntent = Intent(this, ModifyActivity::class.java)
+                startActivity(modifyIntent)
+                return true
+            }
+            R.id.menu_delete -> {
+                // 여기에 일기 삭제 로직을 구현하세요.
+                // 삭제 후 필요한 작업을 수행하세요.
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
